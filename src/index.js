c50a3f8dc20a04121ee1358d3060c2f61c387f82
nilubisan <r.nas9329@gmail.com>
2020-09-29 22:55:17 +0600

    feat: complete typical-arrays-problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (arguments.length == 0 || array.length == 0) return 0;
+return array.reduce(
+    (prev,curr) => {
+      return curr < prev ? curr : prev;
+    } 
+);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (arguments.length == 0 || array.length == 0) return 0;
+return array.reduce(
+    (prev,curr) => {
+      return curr > prev ? curr : prev;
+    } 
+);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+if (arguments.length == 0 || array.length == 0) return 0;
+let sum = array.reduce((prev,curr) => curr + prev);
+return sum/array.length;  
 }
